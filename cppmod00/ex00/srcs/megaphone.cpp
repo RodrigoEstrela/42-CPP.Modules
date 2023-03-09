c5625847ff56db78b27c82bfcbe56e757490ab04
Rodrigo Das neves medina de carvalho estrela <rdas-nev@c2r8s2.42lisboa.com>
2023-03-09 13:32:35 +0000

    mod00ex00

---
+++ cppmod00/ex00/srcs/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	while (i < argc)
+	{
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}

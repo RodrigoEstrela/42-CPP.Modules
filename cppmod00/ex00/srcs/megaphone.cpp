e4b05d2ccf51533293b6194b2c61faacfc6d53c4
rdas-nev <rdas-nev@c2r8s3.42lisboa.com>
2023-03-10 11:16:05 +0000

    added expected behavior for 0 args

--- cppmod00/ex00/srcs/megaphone.cpp
+++ cppmod00/ex00/srcs/megaphone.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cctype>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	int i = 1;
-	while (i < argc)
+	std::string str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	if (ac == 1)
+		std::cout << str;
+	while (i < ac)
 	{
-		for (int j = 0; argv[i][j]; j++)
-			std::cout << (char)toupper(argv[i][j]);
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)toupper(av[i][j]);
 		i++;
 	}
 	std::cout << std::endl;

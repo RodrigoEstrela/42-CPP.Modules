a7747b35ad15075e225125cda24803915773cd78
rdas-nev <restrela20@gmail.com>
2023-03-14 15:49:05 +0000

    ex04

---
+++ ex04/inc/replace_to_file.h
@@ -0,0 +1,14 @@
+//
+// Created by rdas-nev on 14-03-2023.
+//
+
+#ifndef EX04_REPLACE_TO_FILE_H
+#define EX04_REPLACE_TO_FILE_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+void replace_to_file(std::string& line, const std::string& s1, const std::string& s2);
+
+#endif //EX04_REPLACE_TO_FILE_H

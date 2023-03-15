9fc14f682a7c3cf0f30334e0a0a08ba54d95cd1a
rdas-nev <rdas-nev@c2r8s2.42lisboa.com>
2023-03-15 16:42:15 +0000

    formating on .h

--- cppmod02/ex01/inc/Fixed.h
+++ cppmod02/ex01/inc/Fixed.h
@@ -9,19 +9,19 @@
 
 class Fixed
 {
-	int fixed_point;
-	static const int fractional_bits = 8;
-public:
-	Fixed(); // default constructor
-	Fixed(const Fixed &copy); // copy constructor
-	Fixed(int const n); // int constructor
-	Fixed(float const f); // float constructor
-	Fixed &operator=(const Fixed &copy); // copy assignment operator
-	int getRawBits() const;
-	void setRawBits(int const raw);
-	float toFloat() const;
-	int toInt() const;
-	~Fixed();
+		int fixed_point;
+		static const int fractional_bits = 8;
+	public:
+		Fixed(); // default constructor
+		Fixed(const Fixed &copy); // copy constructor
+		Fixed(int const n); // int constructor
+		Fixed(float const f); // float constructor
+		Fixed &operator=(const Fixed &copy); // copy assignment operator
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		float toFloat() const;
+		int toInt() const;
+		~Fixed();
 };
 
 std::ostream &operator<<(std::ostream &out, const Fixed &f);

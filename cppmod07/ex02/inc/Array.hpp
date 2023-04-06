fe2560febb76d625d3f4b8cf4870a5a5a2cb83a3
rdas-nev <restrela20@gmail.com>
2023-04-06 15:21:15 +0100

    mod07ex02

--- cppmod07/ex02/inc/Array.hpp
+++ cppmod07/ex02/inc/Array.hpp
@@ -8,19 +8,49 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename T> class Array
+// Class template to create an array of any type ==============================
+template <typename T>
+class Array
 {
-	private:
-		T *_array;
-		unsigned int _size;
-	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const &other);
-		Array &operator=(Array const &other);
-		~Array();
-		T &operator[](unsigned int index);
-		unsigned int size() const;
+    private:
+        T *_arr;
+        unsigned int _size;
+    public:
+    // Constructor with no parameters -----------------------------------------
+        Array(void) : _arr(NULL), _size(0) {}
+    // Constructor with an unsigned int parameter -----------------------------
+        Array(unsigned int n) : _arr(new T[n]()), _size(n) {}
+    // Copy constructor -------------------------------------------------------
+        Array(const Array &src) : _arr(new T[src._size]()), _size(src._size)
+        {
+            for (unsigned int i = 0; i < _size; i++)
+                _arr[i] = src._arr[i];
+        }
+    // Assignment operator overload -------------------------------------------
+        Array &operator=(const Array &src)
+        {
+            if (this != &src)
+            {
+                delete[] _arr;
+                _arr = new T[src._size]();
+                _size = src._size;
+                for (unsigned int i = 0; i < _size; i++)
+                    _arr[i] = src._arr[i];
+            }
+            return *this;
+        }
+    // Destructor -------------------------------------------------------------
+        ~Array(void) { delete[] _arr; }
+    // Index operator overload ------------------------------------------------
+    T &operator[](unsigned int index)
+        {
+            if (index >= _size)
+                throw std::out_of_range("Index is out of range");
+            return _arr[index];
+        }
+        // Getters ------------------------------------------------------------
+        unsigned int size(void) const { return _size; }
 };
+// ============================================================================
 
 #endif //INC_42_CPP_MODULES_ARRAY_HPP
--- cppmod07/ex02/srcs/main.cpp
+++ cppmod07/ex02/srcs/main.cpp
@@ -6,10 +6,40 @@
 
 int main()
 {
-	Array<int> arr(5);
-	for (unsigned int i = 0; i < arr.size(); i++)
-		arr[i] = i;
-	for (unsigned int i = 0; i < arr.size(); i++)
-		std::cout << arr[i] << std::endl;
-	return 0;
+    // Default constructor -----------------------------------------------------
+    Array<int> arr1;
+    std::cout << "arr1 size: " << arr1.size() << std::endl;
+    // Constructor with an unsigned int parameter ------------------------------
+    Array<int> arr2(5);
+    std::cout << "arr2 size: " << arr2.size() << std::endl;
+    // Copy constructor --------------------------------------------------------
+    Array<int> arr3(arr2);
+    std::cout << "arr3 size: " << arr3.size() << std::endl;
+    for (unsigned int i = 0; i < arr3.size(); i++)
+        std::cout << arr3[i] << " ";
+    std::cout << std::endl;
+    // Assignment operator overload --------------------------------------------
+    Array<int> arr4;
+    arr4 = arr3;
+    std::cout << "arr4 size: " << arr4.size() << std::endl;
+    for (unsigned int i = 0; i < arr4.size(); i++)
+        std::cout << arr4[i] << " ";
+    std::cout << std::endl;
+    // Index operator overload -------------------------------------------------
+    try
+    {
+        arr2[-5] = 1;
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    for (unsigned int i = 0; i < arr2.size(); i++)
+    {
+        arr2[i] = i;
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }

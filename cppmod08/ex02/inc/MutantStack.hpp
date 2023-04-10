0b6f0523fcf48fb2f49a285b961702bd81ebb875
rdas-nev <rdas-nev@c1r14s1.42lisboa.com>
2023-04-10 13:16:18 +0100

    mod08ex02

--- cppmod08/ex02/inc/MutantStack.hpp
+++ cppmod08/ex02/inc/MutantStack.hpp
@@ -7,6 +7,13 @@
 
 #include <iostream>
 #include <stack>
+#include <list>
+
+#define BLUE    "\033[34m"      /* Blue */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define GREEN   "\033[32m"      /* Green */
+#define RED     "\033[31m"      /* Red */
+#define RESET   "\033[0m"
 
 template <typename T>
 class MutantStack : public std::stack<T>
--- cppmod08/ex02/srcs/main.cpp
+++ cppmod08/ex02/srcs/main.cpp
@@ -6,51 +6,69 @@
 
 int main()
 {
+	// MutantStack ------------------------------------------------------------
     {
         MutantStack<int> mstack;
-        mstack.push(5);
-        mstack.push(17);
-        std::cout << mstack.top() << std::endl;
-        mstack.pop();
-        std::cout << mstack.size() << std::endl;
-        mstack.push(3);
-        mstack.push(5);
-        mstack.push(737);
-//[...]
-        mstack.push(0);
-        MutantStack<int>::iterator it = mstack.begin();
-        MutantStack<int>::iterator ite = mstack.end();
-        ++it;
-        --it;
-        while (it != ite)
+		if (mstack.empty())
+			std::cout << RED"Stack is empty." << std::endl;
+        mstack.push(5); std::cout << GREEN"Added one value." << std::endl;
+        mstack.push(17); std::cout << "Added one value." << std::endl;
+        std::cout << YELLOW"Value at the top: " RESET << mstack.top() << std::endl;
+
+		mstack.pop(); std::cout << RED"Removed one value." << std::endl;
+        std::cout << BLUE"Size: " RESET << mstack.size() << std::endl;
+
+		mstack.push(3); std::cout << GREEN"Added one value." << std::endl;
+		mstack.push(5); std::cout << "Added one value." << std::endl;
+		mstack.push(737); std::cout << "Added one value." << std::endl;
+		mstack.push(0);	std::cout << "Added one value." << std::endl;
+		std::cout << BLUE"Size: " RESET << mstack.size() << std::endl;
+
+		MutantStack<int>::iterator it_begin = mstack.begin();
+        MutantStack<int>::iterator it_end = mstack.end();
+        ++it_begin;
+        --it_begin;
+        while (it_begin != it_end)
         {
-            std::cout << *it << std::endl;
-            ++it;
+            std::cout << *it_begin << std::endl;
+            ++it_begin;
         }
         std::stack<int> s(mstack);
+		std::cout << YELLOW"Size of the copied stack: " RESET << s.size() << std::endl;
+		mstack.pop(); std::cout << RED"Removed one value." << std::endl;
+		mstack.pop(); std::cout << "Removed one value." << std::endl;
+		MutantStack<int> mst(mstack);
+		std::cout << YELLOW"Size of the copied mutant stack: " RESET << mst.size() << std::endl;
     }
+	std::cout << "------------------" << std::endl;
+	// Normal List ------------------------------------------------------------
     {
-        std::list<int> mstack;
-        mstack.push_back(5);
-        mstack.push_back(17);
-        std::cout << mstack.back() << std::endl;
-        mstack.pop_back();
-        std::cout << mstack.size() << std::endl;
-        mstack.push_back(3);
-        mstack.push_back(5);
-        mstack.push_back(737);
-//[...]
+        std::list<int> normal_list;
+		if (normal_list.empty())
+			std::cout << RED"List is empty." << std::endl;
+		normal_list.push_back(5); std::cout << GREEN"Added one value." << std::endl;
+		normal_list.push_back(17); std::cout << "Added one value." << std::endl;
+        std::cout << YELLOW"Value at the top: " RESET << normal_list.back() << std::endl;
 
-            mstack.push_back(0);
-            std::list<int>::iterator it = mstack.begin();
-            std::list<int>::iterator ite = mstack.end();
-            ++it;
-            --it;
-            while (it != ite)
-            {
-                std::cout << *it << std::endl;
-                ++it;
-            }
-            std::stack<int> s(mstack);
+		normal_list.pop_back(); std::cout << RED"Removed one value." << std::endl;
+        std::cout << BLUE"Size: " RESET << normal_list.size() << std::endl;
+
+		normal_list.push_back(3); std::cout << GREEN"Added one value." << std::endl;
+		normal_list.push_back(5); std::cout << "Added one value." << std::endl;
+		normal_list.push_back(737); std::cout << "Added one value." << std::endl;
+		normal_list.push_back(0); std::cout << "Added one value." << std::endl;
+		std::cout << YELLOW"Size: " RESET << normal_list.size() << std::endl;
+
+		std::list<int>::iterator it_begin = normal_list.begin();
+		std::list<int>::iterator it_end = normal_list.end();
+		++it_begin;
+		--it_begin;
+		while (it_begin != it_end)
+		{
+			std::cout << *it_begin << std::endl;
+			++it_begin;
+		}
+		std::list<int> lst(normal_list);
+		std::cout << YELLOW"Size of the copied list: " RESET << lst.size() << std::endl;
     }
 }

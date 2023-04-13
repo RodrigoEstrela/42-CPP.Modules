1c607a696fc129b7284a37eb17393da850dcfd60
rdas-nev <restrela20@gmail.com>
2023-04-13 23:34:45 +0100

    mod09ex02v1

--- cppmod09/ex02/srcs/PmergeMe.cpp
+++ cppmod09/ex02/srcs/PmergeMe.cpp
@@ -4,3 +4,118 @@
 
 #include "../inc/PmergeMe.hpp"
 
+// Orthodox Canonical Form ====================================================
+PmergeMe::PmergeMe()
+{
+    v_container = std::vector<int>();
+    d_container = std::deque<int>();
+}
+
+PmergeMe::PmergeMe(const PmergeMe &other)
+{
+    *this = other;
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    this->v_container = other.v_container;
+    this->d_container = other.d_container;
+    return *this;
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+// ============================================================================
+
+// Algorithm For Vector Container =============================================
+void PmergeMe::v_merge_insert_sort(int start, int end)
+{
+    if (start >= end)
+        return;
+    if (end - start + 1 <= 16)
+    {
+        insertion_sort(v_container, start, end);
+        return;
+    }
+    int mid = start + (end - start) / 2;
+    v_merge_insert_sort(start, mid);
+    v_merge_insert_sort(mid+1, end);
+    std::vector<int> temp(end - start + 1);
+    int i = start, j = mid+1, k = 0;
+    while (i <= mid && j <= end)
+    {
+        if (v_container[i] <= v_container[j])
+            temp[k++] = v_container[i++];
+        else
+            temp[k++] = v_container[j++];
+    }
+    while (i <= mid)
+        temp[k++] = v_container[i++];
+    while (j <= end)
+        temp[k++] = v_container[j++];
+    for (int l = 0; l < k; l++)
+        v_container[start + l] = temp[l];
+}
+
+void PmergeMe::v_merge_insert_sort()
+{
+    v_merge_insert_sort(0, (int)v_container.size() - 1);
+}
+// ============================================================================
+
+// Algortihm For Deque Container ==============================================
+void PmergeMe::d_merge_insert_sort(int start, int end)
+{
+    if (end - start <= 1) {
+        return;
+    }
+    if (end - start <= 8) {
+        insertion_sort(d_container, start, end);
+        return;
+    }
+    int mid = (start + end) / 2;
+    d_merge_insert_sort(start, mid);
+    d_merge_insert_sort(mid, end);
+    std::deque<int>::iterator it1 = d_container.begin() + start;
+    std::deque<int>::iterator it2 = d_container.begin() + mid;
+    while (it1 != d_container.begin() + mid && it2 != d_container.begin() + end)
+    {
+        if (*it1 < *it2)
+        {
+            it1++;
+        }
+        else
+        {
+            int value = *it2;
+            d_container.erase(it2);
+            d_container.insert(it1, value);
+            it1++;
+            it2++;
+            mid++;
+        }
+    }
+}
+
+void PmergeMe::d_merge_insert_sort()
+{
+    d_merge_insert_sort(0, (int)d_container.size() - 1);
+}
+// ============================================================================
+
+// Class Exceptions ===========================================================
+const char *PmergeMe::InvalidArgument::what() const throw()
+{
+    return "Error: invalid argument.";
+}
+
+const char *PmergeMe::HasDuplicate::what() const throw()
+{
+    return "Error: has duplicates.";
+}
+
+const char *PmergeMe::HasNegative::what() const throw()
+{
+    return "Error: has negative number.";
+}
+// ============================================================================
--- cppmod09/ex02/srcs/main.cpp
+++ cppmod09/ex02/srcs/main.cpp
@@ -6,5 +6,45 @@
 
 int main(int ac, char **av)
 {
-
+    if (ac >= 3)
+    {
+        PmergeMe sorter;
+        try
+        {
+            // Input --------------------------------------------------------
+            std::vector<int> input;
+            sorter.fill_array_from_args(ac, av, input);
+            std::cout << "Original array: ";
+            for (int i = 0; i < (int)input.size(); i++)
+                std::cout << input[i] << " ";
+            std::cout << std::endl;
+            // Vector Container ---------------------------------------------
+            std::clock_t start = std::clock();
+            sorter.fill_array_from_args(ac, av, sorter.v_container);
+            sorter.v_merge_insert_sort();
+            std::clock_t end = std::clock();
+            // Printing Sorted ---------------------------------------------
+            std::cout << "Sorted array: ";
+            for (int i = 0; i < (int)sorter.v_container.size(); i++)
+                std::cout << sorter.v_container[i] << " ";
+            std::cout << std::endl;
+            // Deque Container ----------------------------------------------
+            std::clock_t startd = std::clock();
+            sorter.fill_array_from_args(ac, av, sorter.d_container);
+            sorter.d_merge_insert_sort();
+            std::clock_t endd = std::clock();
+            // Printing Times ------------------------------------------------
+            std::cout << "Vector: " << (end - start) * 1000000 / CLOCKS_PER_SEC << " microseconds" << std::endl;
+            std::cout << "Deque: " << (endd - startd) * 1000000 / CLOCKS_PER_SEC << " microseconds" << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cout << e.what() << std::endl;
+        }
+    }
+    else
+    {
+        std::cout << "Insufficient arguments." << std::endl;
+        return 1;
+    }
 }
